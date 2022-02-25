ddcbd737c65a80d83e458e66f1bf022ded68c003
zhangguanwen <970581469@qq.com>
2022-02-25 15:40:46 +0800

    submit LoRaWAN_End_Node project

---
+++ Drivers/BSP/STM32WLxx_LoRa_E5_mini/stm32wlxx_LoRa_E5_mini_conf_template.h
@@ -0,0 +1,76 @@
+/**
+  ******************************************************************************
+  * @file    stm32wlxx_LoRa_E5_mini_conf.h
+  * @author  MCD Application Team
+  * @brief   STM32WLxx_LoRa_E5_mini board configuration file.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2020(-2021) STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32WLXX_LORA_E5_MINI_CONF_H
+#define STM32WLXX_LORA_E5_MINI_CONF_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @addtogroup STM32WLXX_LoRa_E5_mini
+  * @{
+  */
+
+/** @defgroup STM32WLXX_LoRa_E5_mini_CONFIG CONFIG
+  * @{
+  */
+
+/** @defgroup STM32WLXX_LoRa_E5_mini_CONFIG_Exported_Constants Exported Constants
+  * @{
+  */
+/* COM usage define */
+#define USE_BSP_COM_FEATURE                 0U
+
+/* COM log define */
+#define USE_COM_LOG                         0U
+
+/* IRQ priorities */
+#define BSP_BUTTON_SWx_IT_PRIORITY         15U
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STM32WLXX_LoRa_E5_mini_CONF_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Projects/Applications/LoRaWAN/LoRaWAN_End_Node/Core/Inc/main.h
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RTC_N_PREDIV_S 10
+#define RTC_PREDIV_S ((1<<RTC_N_PREDIV_S)-1)
+#define RTC_PREDIV_A ((1<<(15-RTC_N_PREDIV_S))-1)
+
+#define USART_BAUDRATE 115200
+
+#define LED1_Pin GPIO_PIN_5
+#define LED1_GPIO_Port GPIOB
+
+#define BUT1_Pin GPIO_PIN_13
+#define BUT1_GPIO_Port GPIOB
+#define BUT1_EXTI_IRQn EXTI15_10_IRQn
+
+#define RF_CTRL1_Pin GPIO_PIN_4
+#define RF_CTRL1_GPIO_Port GPIOA
+
+#define RF_CTRL2_GPIO_Port GPIOA
+#define RF_CTRL2_Pin GPIO_PIN_5
+
+#define USARTx_RX_Pin GPIO_PIN_6
+#define USARTx_RX_GPIO_Port GPIOB
+
+#define USARTx_TX_Pin GPIO_PIN_7
+#define USARTx_TX_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
