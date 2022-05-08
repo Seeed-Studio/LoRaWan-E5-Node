bb722349b886cdb06516228ad56c3d9a132ace2a
Rory Piper <rory.piper@signal-fire.com>
2022-05-08 07:57:37 -0400

    build: Fix multiple definitions of hlptim1

--- Projects/Applications/FreeRTOS/FreeRTOS_LowPower/Core/Inc/main.h
+++ Projects/Applications/FreeRTOS/FreeRTOS_LowPower/Core/Inc/main.h
@@ -59,7 +59,7 @@ void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
 /* USER CODE BEGIN Private defines */
-LPTIM_HandleTypeDef hlptim1;
+extern LPTIM_HandleTypeDef hlptim1;
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
